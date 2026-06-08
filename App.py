ecd7e267cf12a19aeee792c044c51a3ead8b714f
babu961 <babureddy18042006@gmail.com>
2026-06-08 17:18:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
-import streamlit as st
 
-st.set_page_config(
-    page_title="Stock Analytics Pro",
-    page_icon="📊",
-    layout="wide"
-)
 st.set_page_config(
     page_title="Stock Analytics Pro",
     page_icon="📈",
